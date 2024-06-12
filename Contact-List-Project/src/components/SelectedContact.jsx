37fce492d9176f487f7f481b1aeba853eea50dee
kiannadalton <kiannadalton@aol.com>
2024-06-12 18:54:18 -0400

    added update

--- Contact-List-Project/src/components/SelectedContact.jsx
+++ Contact-List-Project/src/components/SelectedContact.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ContactList from "./ContactList";
 
 
-export default function SelectedContact ({ ContactList.id }){
+export default function SelectedContact (){
     const [selectedContactId, setSelectedContactId] = useState(ContactList);
 
+    useEffect(()=>{
+        async function fetchContacts(){
+            try {
+                const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+                const result = await response.json();
+                setContacts(result);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        // need to call fetchContacts to make it work
+        fetchContacts();
+    },[]);
 
     return (<p onClick={() => {selectedContactId(ContactList.id)}} key={ContactList.id}>
     {ContactList.name}
