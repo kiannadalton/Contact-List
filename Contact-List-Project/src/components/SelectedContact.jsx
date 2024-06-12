01ddeee31e52cd5ed483b2743a82b0384b650278
kiannadalton <kiannadalton@aol.com>
2024-06-12 18:32:16 -0400

    Adding updated code

--- Contact-List-Project/src/components/SelectedContact.jsx
+++ Contact-List-Project/src/components/SelectedContact.jsx
@@ -3,26 +3,26 @@ import ContactList from "./ContactList";
 
 
 
-export default function selectedContactId ({ contact }){
-    const [Selected, setSelectedContactId] = useState(ContactList);
-    
+export default function SelectedContact ({ contact }){
+    const [selectedContactId, setSelectedContactId] = useState(ContactList);
 
-    return (<p onClick={() => {selectedContactId(contact.id)}} key={contact.id}>
-    {contact.name}
+
+    return (<p onClick={() => {selectedContactId(ContactList.id)}} key={ContactList.id}>
+    {ContactList.name}
   </p>
 
 );
 <h1> Selected Contact:</h1>
-{ featPupId && (
-   <div>
-    <h2>{featuredPup.name}</h2>
-    <ul>
-      <li>Name: {contact.name}</li>
-      <li>Email: {contact.email}</li>
-      <li>Email: {contact.phone}</li>
+// { featPupId && (
+//    <div>
+//     <h2>{featuredPup.name}</h2>
+//     <ul>
+//       <li>Name: {contact.name}</li>
+//       <li>Email: {contact.email}</li>
+//       <li>Email: {contact.phone}</li>
 
-    </ul>
-   </div>  
-  )}
+//     </ul>
+//    </div>  
+//   )}
 
 }
